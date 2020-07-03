d21c01fff5b8b68a48d03bc0ce73461978e612df
EDZ <EDZ@WINDOWS-EVIAJKI>
2020-07-03 14:31:47 +0800

    继承thread类，两种调用方式

---
+++ TestWeb/src/com/test/ThreadTest2.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class ThreadTest2 extends Thread{
+	@Override
+	public void run() {
+		for(int i=0;i<10;i++) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		//第一种启动Thread的子类中重写的run();
+//		new ThreadTest2().run();
+		
+		//第二种启动ThreadTest2线程
+		new ThreadTest2().start();
+	}
+}
