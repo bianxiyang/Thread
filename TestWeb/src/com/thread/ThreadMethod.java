4cecc477636557f702dbc8db38d4c9a909e9a7dc
Administrator <Administrator@192.168.1.105>
2020-07-03 23:02:28 +0800

    线程的常用方法sleep，yield，join

--- TestWeb/src/com/thread/ThreadMethod.java
+++ TestWeb/src/com/thread/ThreadMethod.java
@@ -1,36 +1,71 @@
 package com.thread;
+
+import java.util.Random;
+
 /**
  * 
  * 
- *	线程常用方法
- *	Thread.sleep(5000):线程循环调用CPU执行，当前线程睡眠5000毫秒，让给其他线程来调用CPU
- *	Thread.yield():暂时让出一下CPU，进入到等待队列里
- *	join:加入到当前线程中，常用于等待一个线程的结束，面试题（保证三个线程按顺序执行，t1执行后，joint2,t2执行完执行t3,或者t1.join()--->t2.join()--->t3.join()）
+ * 线程常用方法 Thread.sleep(5000):线程循环调用CPU执行，当前线程睡眠5000毫秒，让给其他线程来调用CPU
+ * Thread.yield():暂时让出一下CPU，进入到等待队列里
+ * join:加入到当前线程中，常用于等待一个线程的结束，面试题（保证三个线程按顺序执行，t1执行后，joint2,t2执行完执行t3,或者t1.join()--->t2.join()--->t3.join()）
  *
  *
  */
 
-public class ThreadMethod extends Thread{
-	@Override
-	public void run() {
-		while(true) {
-			try {
-				//线程的休眠方法
-				Thread.sleep(5000);
-				System.out.println(ThreadMethod.class.getName());
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+public class ThreadMethod {
+	
+	
+		Thread t1 = new Thread() {
+			@Override
+			public void run() {
+				for(int i=0;i<10;i++) {
+					
+					System.out.println("t1");
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
-			
-		}
-	}
+		};
+		
+		
+		
+		
+		
+		Thread t2 = new Thread() {
+			public void run() {
+				for(int j=0;j<10;j++) {
+					System.out.println("t222222");
+					if(j==5) {
+						try {
+							t1.join();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			};
+		};
+		
+		
+		
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		
-		ThreadMethod method = new ThreadMethod();
-		method.start();
-		
+		ThreadMethod m = new ThreadMethod();
+		m.t1.start();
+		m.t2.start();
 	}
+	
+	
 	
 }
