ea9a75452e596fe4b0b78cf3d44a37b8d579b32c
EDZ <EDZ@WINDOWS-EVIAJKI>
2020-07-03 14:38:39 +0800

    runnable接口

--- TestWeb/src/com/test/ThreadTest3.java
+++ TestWeb/src/com/test/ThreadTest3.java
@@ -1,12 +1,16 @@
 package com.test;
 /**
- *	继承Thread类，两种调用方式
+ *
+ *	实现runnable接口
  *
  */
-public class ThreadTest2 extends Thread{
+public class ThreadTest3 implements Runnable{
+
 	@Override
 	public void run() {
+		
 		for(int i=0;i<10;i++) {
+			
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
@@ -16,13 +20,14 @@ public class ThreadTest2 extends Thread{
 		}
 	}
 	
-	
-	
 	public static void main(String[] args) {
-		//第一种启动Thread的子类中重写的run();
-//		new ThreadTest2().run();
 		
-		//第二种启动ThreadTest2线程
-		new ThreadTest2().start();
+		new ThreadTest3().run();
+		
+		
+		
 	}
+	
+	
+	
 }
