5b4884c0d3ad6a51bd3839f432614cacd432a804
EDZ <EDZ@WINDOWS-EVIAJKI>
2020-07-03 13:54:53 +0800

    添加了多种线程的方式

--- TestWeb/src/com/test/ThreadTest.java
+++ TestWeb/src/com/test/ThreadTest.java
@@ -3,6 +3,7 @@ package com.test;
 public class ThreadTest {
 	public static void main(String[] args) {
 		
+		//第一种创建线程的方法
 		Thread thread = new Thread(){
 			@Override
 			public void run() {
@@ -13,11 +14,41 @@ public class ThreadTest {
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					System.out.println("2321321");
+					System.out.println("thread1");
 				}
 				
 			}
 		};
 		thread.start();
+		
+		
+		
+		//第二种创建线程
+		
+		Thread thread2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				while(true) {
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("thread2");
+				}
+			}
+		}) {
+			
+		};
+		
+		thread2.start();
+		
+		
 	}
+	
+	
+	
+	
 }
