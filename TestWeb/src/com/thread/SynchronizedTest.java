53826bf7d5a54e3feadfea4b0e33d2bf16aea8d9
Administrator <Administrator@192.168.1.105>
2020-07-04 21:08:28 +0800

    nothing

--- TestWeb/src/com/thread/SynchronizedTest.java
+++ TestWeb/src/com/thread/SynchronizedTest.java
@@ -1,20 +1,37 @@
 package com.thread;
-
-public class SynchronizedTest {
+/**
+ * 
+ * 	锁，循环对count--,--的同时要锁定当前操作，完成后才可以让其他线程进行操作
+ * 
+ * 
+ * @author bian_xy
+ *
+ */
+public class SynchronizedTest implements Runnable{
 	
 	private int count = 10;
-	private Object o = new Object();
+	
+	
+	@Override
+	public  synchronized void run() {
+		count--;
+		System.out.println(Thread.currentThread().getName()+" count="+ count);
+	}
 	
 	
 	
-	public  void m() {
-		synchronized (o) {
-			
-			count--;
-			System.out.println(Thread.currentThread().getName()+" count="+count);
-			
+	
+	public static void main(String[] args) {
+		
+		
+		SynchronizedTest test = new SynchronizedTest();
+		
+		for(int i=0;i<10;i++) {
+
+			new Thread(test).start();
 		}
 		
 		
 	}
+	
 }
